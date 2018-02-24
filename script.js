ee21791781ba80ffc4bba818982700498a189bf7
Szymon Oleszek <szymqwerty@gmail.com>
2018-02-24 16:59:23 +0100

    almost finish

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
     var points
     var time
     var mole
+    var gameIntervalId
 
 
     function addPoint(){
+       
         points++
-        console.log('dodano punkt', points)
+        console.log('dzialam')
         displayPoints(points)
         
     }
@@ -14,15 +16,24 @@
     function displayPoints(pointsParam){
 
         var pointsContainer = document.querySelector('.points')
-        pointsContainer.innerText = pointsParam
+        pointsContainer.innerText = 'Punkty: ' + pointsParam
 
+
+    }
+
+    function reduceTime(){
+
+
+        time--
+        displayTime(time)
+        if(time === 0) endGame()
 
     }
 
     function displayTime(timeParam){
 
         var timeContainer = document.querySelector('.time')
-        timeContainer.innerText = timeParam
+        timeContainer.innerText = 'Czas: ' + timeParam
 
     }
 
@@ -50,6 +61,7 @@
                 function(){
                     mole.remove()
                     addPoint()
+                    flashBackground()
 
 
 
@@ -60,7 +72,26 @@
             return mole
     }
 
+    function endGame(){
+            clearInterval(gameIntervalId)
+            mole.remove()
+            alert('game was ended!\nYour score was: ' + points + ' !')
 
+
+    }
+
+
+    function flashBackground(){
+        var body = document.querySelector('body')
+        body.style.backgroundColor = 'red'
+        setTimeout(
+            function(){
+                body.style.backgroundColor = 'green'
+            }, 100
+        )
+
+
+    }
 
     function init(){
 
@@ -68,8 +99,22 @@
         time = 10 
         mole = makeMole()
 
-        displayPoints("punkty: " + points)
-        displayTime("czas:  " + time)
+        displayPoints(points)
+        displayTime(time)
+ 
+
+        gameIntervalId = setInterval(
+            function(){
+
+                    mole.remove()
+                    mole = makeMole()
+                    reduceTime()
+                    
+            },
+
+            3000
+
+        )
 
 
     }
