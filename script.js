aa2aa6d5d205be23569353a543af1479423a23e2
Szymon Oleszek <szymqwerty@gmail.com>
2018-02-24 12:50:42 +0100

    poznanie typow

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
 console.log('Hello Szymon!')
-
+//string
 var textVariable = "Szymon"
-
+//number
+var numberVariable = 43
 console.log(textVariable)
+console.log(numberVariable)
+
+var boolVariable = true
+
+console.log(boolVariable)
+
+//null
+var nullVariable = null
+console.log(nullVariable)
+
+//undefined
+var undefinedVariable = undefined
+console.log(undefinedVariable)
+
+//objects
+
+var simplestObj = {
+    name: "Szymon",
+    lastname: "Oleszek"
+
+
+}
+
+var nastedObj = {
+
+    name: "Szymon",
+
+    car: {
+
+        brand: "fiat",
+        model: "panda"
+
+    }
+
+}
+
+console.log(nastedObj.car.model)
