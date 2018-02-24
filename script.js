f6cc9aa4fc64a750561b7c3b854d92047f5555ba
Szymon Oleszek <szymqwerty@gmail.com>
2018-02-24 15:36:54 +0100

    mole exist

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-var body= document.querySelector('body')
+(function(){
 
-console.log(body)
+    function makeMole(){
 
-body.addEventListener(
-    
-    'click',
 
-    function(){
-        console.log('klik')
+            var molePosX = Math.round(
+                Math.random() * (window.innerWidth - window.innerHeight / 10)
+            )
+            var molePosY = Math.round(
+                Math.random() * (window.innerHeight - window.innerHeight / 10)
+            )
 
+            var mole = document.createElement('div')
+
+            mole.classList.add('mole')
+
+            mole.style.left = molePosX + 'px'
+            mole.style.top = molePosY + 'px'
+
+            document.querySelector('body').appendChild(mole)
     }
-)
+
+    makeMole()
+
+
+})()
