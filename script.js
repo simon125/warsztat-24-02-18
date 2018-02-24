b4d5fd1e07ce006074eff071f6f06566c7c3327c
Szymon Oleszek <szymqwerty@gmail.com>
2018-02-24 13:17:20 +0100

    petla for

--- script.js
+++ script.js
@@ -1,52 +1,14 @@
-console.log('Hello Szymon!')
-//string
-var textVariable = "Szymon"
-//number
-var numberVariable = 43
-console.log(textVariable)
-console.log(numberVariable)
 
-var boolVariable = true
-
-console.log(boolVariable)
-
-//null
-var nullVariable = null
-console.log(nullVariable)
-
-//undefined
-var undefinedVariable = undefined
-console.log(undefinedVariable)
-
-//objects
-
-var simplestObj = {
-    name: "Szymon",
-    lastname: "Oleszek"
+function hello(name, j){
 
 
-}
 
-var nastedObj = {
+    for(var i = 0 ; i<j; i++){
 
-    name: "Szymon",
-
-    car: {
-
-        brand: "fiat",
-        model: "panda"
+        console.log('hello ' + name)
 
     }
+ }
 
-}
-
-console.log(nastedObj.car.model)
-
-
-var simplestArray = [{name: "Ala", wiek: 20},2,3,4,5]
-
-
-console.log(simplestArray)
-console.log(simplestArray[3])
-console.log(simplestArray[0].wiek)
-
+ //function invocation (call)
+ hello('Szymon', 7)
